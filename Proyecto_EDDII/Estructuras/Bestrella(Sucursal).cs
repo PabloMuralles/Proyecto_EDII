a71c93f2a6daf79f9c665299dde826414930e434
Santiagobocel <santiagobocel10@gmail.com>
2020-03-22 14:44:04 -0600

    arbol Sucursal insertar

---
+++ Proyecto_EDDII/Estructuras/Bestrella(Sucursal).cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_EDDII.Estructuras
+{
+    public class Bestrella_Sucursal_
+    {
+        private static Bestrella_Sucursal_ _instance = null;
+        public static Bestrella_Sucursal_ Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Bestrella_Sucursal_();
+                return _instance;
+            }
+        }
+        Nodo raiz = null;
+        public bool entrar = true;
+        static int grado = 7;
+        int identificador = 1;
+        static int valor = ((4 * (grado - 1)) / 3);
+        public void Insertar(int ID, string Name, string Adress)
+        {
+            if (entrar)
+            {
+                raiz = new Nodo(grado,entrar);
+                entrar = false;
+                raiz.values[0] = new Sucursal()
+                {
+                    ID = ID,
+                    Nombre = Name,
+                    direccion = Adress
+                };
+                raiz.ID = identificador;
+            }
+            else
+            {
+                int num = 0;
+                foreach (var espacio in raiz.values)
+                {
+                    if (espacio == null && num < valor)
+                    {
+                        raiz.values[num] = new Sucursal()
+                        {
+                            ID = ID,
+                            Nombre = Name,
+                            direccion = Adress
+                        };
+                        break;
+                    }
+                    num++;
+                    if (num == valor) /// full
+                    {
+
+                    }
+                }
+                raiz.values = Ordenar(raiz.values);
+            }
+        }
+
+        public Sucursal[] Ordenar(Sucursal[] valores)
+        {
+            var lista = new List<Sucursal>();
+            foreach (var iteraciones in valores)
+            {
+                if (iteraciones != null)
+                {
+                    lista.Add(iteraciones);
+                }
+            }
+            lista = lista.OrderBy(x => x.Nombre).ToList();
+            var contador = 0;
+            foreach (var item in lista)
+            {
+                valores[contador] = item;
+                contador++;
+            }
+            return valores;
+        }
+    }
+}
---
+++ Proyecto_EDDII/Nodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_EDDII
+{
+    public class Nodo
+    {
+        public int ID {get;set;}
+        public int padre { get; set; }
+        public int[] hijos { get; set; }
+        public Sucursal[] values { get; set; }
+        public Nodo(int grado, bool posicion)
+        {
+          if (posicion)
+            {
+                int valor = ((4 * (grado - 1)) / 3);
+                values = new Sucursal[valor];
+                hijos = new int[grado];
+            }
+            else
+            {
+                values = new Sucursal[grado - 1];
+                hijos = new int[grado];
+            }
+        }
+    }
+}
