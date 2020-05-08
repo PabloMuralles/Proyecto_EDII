4cbb858a180933750e173472dd25f0a12da9a48b
PabloMuralles <pablomuralles28@gmail.com>
2020-05-07 22:15:23 -0600

    se implemento que si el usuario no ingresa la contraseña se genere random y se guarde en un txt dentro de la carpeta contraseña. Tambien se valido que si el archivo viene vacio se genere una contraseña y se escriba en ese archivo

--- Proyecto_EDDII/Configuracion/Configuracion.cs
+++ Proyecto_EDDII/Configuracion/Configuracion.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text;
 
 namespace Proyecto_EDDII.Configuracion
 {
     public class Configuracion
     {
+
+        /// <summary>
+        /// Metodo para aplicar el singleton con su propiedad
+        /// </summary>
         private static Configuracion _instance = null;
         public static Configuracion Instance
         {
@@ -18,31 +23,75 @@ namespace Proyecto_EDDII.Configuracion
             }
         }
 
+        /// <summary>
+        /// Propiedad donde se mantendra la contraseña
+        /// </summary>
         private int contraseña;
 
+        /// <summary>
+        /// Propiedad para que puedan acceder a la contraseña pero sin que la modifiquen
+        /// </summary>
         public int Contaseña { get => contraseña; }
+
+        /// <summary>
+        /// Metodo para poder obtener la contraseña del usuario si el usuario no inserta ninguna se generara una aleatoria escribiendola en la carpeta contraseña en un txt con nombre contraseña
+        /// </summary>
 
         public void LecturaContraseña()
         {
             string Ruta = Environment.CurrentDirectory;
 
             var ContraseñaString = string.Empty;
-
-            using (var Archivo = new FileStream(Path.Combine(Ruta, "Contraseña", "contraseña.txt"), FileMode.Open))
+            if (File.Exists(Path.Combine(Ruta, "Contraseña", "contraseña.txt")) && new FileInfo(Path.Combine(Ruta, "Contraseña", "contraseña.txt")).Length != 0)
             {
-                using (var Lectura = new BinaryReader(Archivo))
+                using (var Archivo = new FileStream(Path.Combine(Ruta, "Contraseña", "contraseña.txt"), FileMode.Open))
                 {
-                     
-                    var buffer = new byte[1];
-                    while (Lectura.BaseStream.Position != Lectura.BaseStream.Length)
+                    using (var Lectura = new BinaryReader(Archivo))
                     {
-                        buffer = Lectura.ReadBytes(1);
 
-                        ContraseñaString += Convert.ToString(Convert.ToChar(buffer[0]));
-                         
+                        var buffer = new byte[1];
+                        while (Lectura.BaseStream.Position != Lectura.BaseStream.Length)
+                        {
+                            buffer = Lectura.ReadBytes(1);
+
+                            ContraseñaString += Convert.ToString(Convert.ToChar(buffer[0]));
+
+                        }
                     }
+
                 }
 
+            }
+            else
+            {
+                var Random = new Random();
+                if (!File.Exists(Path.Combine(Ruta, "Contraseña", "contraseña.txt")))
+                {
+                    using (var Archivo = new FileStream(Path.Combine(Ruta, "Contraseña", "contraseña.txt"), FileMode.Create))
+                    {
+                        using (var Escritura = new BinaryWriter(Archivo))
+                        {
+                            ContraseñaString = Random.Next(0, 1023).ToString();
+                            Escritura.Write(Encoding.UTF8.GetBytes(ContraseñaString));
+                            
+                        }
+
+                    }
+
+                }
+                else if (new FileInfo(Path.Combine(Ruta, "Contraseña", "contraseña.txt")).Length == 0)
+                {
+
+                    using (var Archivo = new FileStream(Path.Combine(Ruta, "Contraseña", "contraseña.txt"), FileMode.Open))
+                    {
+                        using (var Escritura = new BinaryWriter(Archivo))
+                        {
+                            ContraseñaString = Random.Next(0, 1023).ToString();
+                            Escritura.Write(Encoding.UTF8.GetBytes(ContraseñaString));
+                        }
+
+                    }
+                }
             }
 
             contraseña = Convert.ToInt32(ContraseñaString);
